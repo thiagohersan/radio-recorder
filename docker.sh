73c70080d6d6b61d37e6ca0c905114bd3425a7ea
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-07-07 19:10:01 -0300

    fixes docker run command

--- docker.sh
+++ docker.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 docker build -t radio-recorder .
-docker run -d -v $PWD/out:/opt/radio-recorder/out \
--e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_S3_BUCKET \
-radio-recorder
+docker run -d -v $PWD/out:/opt/radio-recorder/out --env-file .env radio-recorder
