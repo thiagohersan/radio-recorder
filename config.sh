5620110603d91c1ad6f40a83c43392e5df89c291
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-07-08 13:52:34 -0300

    updates record and config scripts to show station name on output files

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y wget streamripper
+apt-get install -y zip wget streamripper
 wget https://raw.githubusercontent.com/thiagohersan/radio-recorder/master/record.sh
 chmod 700 record.sh
 chown admin:admin record.sh
