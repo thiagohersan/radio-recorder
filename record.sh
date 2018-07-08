857553732a177c720844bca98b44bb9fcf5cf47b
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-07-08 01:16:34 -0300

    try -A option

--- record.sh
+++ record.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 streamripper 'http://19293.live.streamtheworld.com:3690/JP_NEWSAAC_SC' -u 'iTunes/4.7 (Macintosh; N; PPC)' \
--a %S%q -l 8000 -s -d ./out &
+-A -a %S%q -l 8000 -s -d ./out &
 
 streamripper 'http://usa12.fastcast4u.com:5080/;' -u 'iTunes/4.7 (Macintosh; N; PPC)' \
--a %S%q -l 8000 -s -d ./out &
+-A -a %S%q -l 8000 -s -d ./out &
 
 streamripper 'http://icecast-streaming.nice264.com/mallorca?.mp3' -u 'iTunes/4.7 (Macintosh; N; PPC)' \
--a %S%q -l 8000 -s -d ./out &
+-A -a %S%q -l 8000 -s -d ./out &
 
 streamripper 'http://icestreaming.rai.it/1.mp3' -u 'iTunes/4.7 (Macintosh; N; PPC)' \
--a %S%q -l 8000 -s -d ./out &
+-A -a %S%q -l 8000 -s -d ./out &
 
 streamripper 'http://radiomars.ice.infomaniak.ch/radiomars-128.mp3' -u 'iTunes/4.7 (Macintosh; N; PPC)' \
--a %S%q -l 8000 -s -d ./out
+-A -a %S%q -l 8000 -s -d ./out
 
 rm -rf ./out/*.cue ./out/incomplete
 
