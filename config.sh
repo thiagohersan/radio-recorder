0b14e4592f353212551bdb1b2eb3f765bb18f282
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-07-08 19:22:24 -0300

    adds at to provisioned machine

--- config.sh
+++ config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y zip wget streamripper
+apt-get install -y zip wget at streamripper
+systemctl enable atd.service
 wget https://raw.githubusercontent.com/thiagohersan/radio-recorder/master/record.sh
 chmod 700 record.sh
 chown admin:admin record.sh
