1a3718a83d13d9014de32752bae361243c976d57
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-07-10 17:21:30 -0300

    updates/fixes config file

--- config.sh
+++ config.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y zip wget at streamripper
-systemctl enable atd.service
+sudo apt-get update
+sudo apt-get install -y zip wget at streamripper
+sudo systemctl enable atd.service
 wget https://raw.githubusercontent.com/thiagohersan/radio-recorder/master/record.sh
 chmod 700 record.sh
 chown admin:admin record.sh
+at 14:50 071118 -f ~/record.sh
+at 10:50 071418 -f ~/record.sh
+at 11:50 071518 -f ~/record.sh
