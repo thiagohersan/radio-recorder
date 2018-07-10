0a1d0ee0c17c586f2729821efee6587960a445b9
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-07-10 16:58:58 -0300

    adds new stations. records for 150 minutes.

--- record.sh
+++ record.sh
@@ -1,64 +1,98 @@
 #!/bin/bash
 
-streamripper 'http://mmg.streamguys1.com/AdomFM-mp3?key=93fe20a16f78c70991fa726b9ca9c19c49f7329a3ad6144500e8fe7f3b8dadbafa6e84e66e84f9d149b17181fcf7194f' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a AdomFM_%q -l 8000 -s -d ./out &
+streamripper 'http://mmg.streamguys1.com/AdomFM-mp3?key=93fe20a16f78c70991fa726b9ca9c19c49f7329a3ad6144500e8fe7f3b8dadbafa6e84e66e84f9d149b17181fcf7194f' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a AdomFM_%q -l 9000 -s -d ./out &
 
-streamripper 'http://mmg.streamguys1.com/LuvFM-mp3?key=93fe20a16f78c70991fa726b9ca9c19c49f7329a3ad6144500e8fe7f3b8dadbafa6e84e66e84f9d149b17181fcf7194f' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a LuvFM_%q -l 8000 -s -d ./out &
+streamripper 'http://mmg.streamguys1.com/LuvFM-mp3?key=93fe20a16f78c70991fa726b9ca9c19c49f7329a3ad6144500e8fe7f3b8dadbafa6e84e66e84f9d149b17181fcf7194f' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a LuvFM_%q -l 9000 -s -d ./out &
 
-streamripper 'http://pvstreaming.rri.co.id:9025/rrijakartapro1.mp3' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a rri_%q -l 8000 -s -d ./out &
+streamripper 'http://pvstreaming.rri.co.id:9025/rrijakartapro1.mp3' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a rri_%q -l 9000 -s -d ./out &
 
-streamripper 'http://82.114.72.2:8088/listen.pls' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a 82114722_%q -l 8000 -s -d ./out &
+streamripper 'http://82.114.72.2:8088/listen.pls' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a 82114722_%q -l 9000 -s -d ./out &
 
-streamripper 'http://streams.radioafricagroup.co.ke:88/broadwave.mp3' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a radioafricagroup_%q -l 8000 -s -d ./out &
+streamripper 'http://streams.radioafricagroup.co.ke:88/broadwave.mp3' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a radioafricagroup_%q -l 9000 -s -d ./out &
 
-streamripper 'http://38.107.243.226:8010/listen.pls' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a 38107243226_%q -l 8000 -s -d ./out &
+streamripper 'http://38.107.243.226:8010/listen.pls' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a 38107243226_%q -l 9000 -s -d ./out &
 
-streamripper 'http://live01.kezik.net/sapientia' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a kezik_%q -l 8000 -s -d ./out &
+streamripper 'http://live01.kezik.net/sapientia' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a kezik_%q -l 9000 -s -d ./out &
 
-streamripper 'http://icecast.vgtrk.cdnvideo.ru/rrzonam_mp3_192kbps' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a rrzonam_%q -l 8000 -s -d ./out &
+streamripper 'http://icecast.vgtrk.cdnvideo.ru/rrzonam_mp3_192kbps' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a rrzonam_%q -l 9000 -s -d ./out &
 
-streamripper 'http://streamingv2.shoutcast.com/BeladiFm966' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a BeladiFm966_%q -l 8000 -s -d ./out &
+streamripper 'http://streamingv2.shoutcast.com/BeladiFm966' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a BeladiFm966_%q -l 9000 -s -d ./out &
 
-streamripper 'http://icecast.vgtrk.cdnvideo.ru/mayakfm_mp3_64kbps' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a mayakfm_%q -l 8000 -s -d ./out &
+streamripper 'http://icecast.vgtrk.cdnvideo.ru/mayakfm_mp3_64kbps' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a mayakfm_%q -l 9000 -s -d ./out &
 
-streamripper 'http://sbs-ice.streamguys1.com/sbs2' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a sbs2_%q -l 8000 -s -d ./out &
+streamripper 'http://sbs-ice.streamguys1.com/sbs2' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a sbs2_%q -l 9000 -s -d ./out &
 
-streamripper 'http://ntvsporsc.radyotvonline.com/listen.pls' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a radyotvonline_%q -l 8000 -s -d ./out &
+streamripper 'http://ntvsporsc.radyotvonline.com/listen.pls' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a radyotvonline_%q -l 9000 -s -d ./out &
 
-streamripper 'http://19293.live.streamtheworld.com:3690/JP_NEWSAAC_SC' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a JPNEWS_%q -l 8000 -s -d ./out &
+streamripper 'http://19293.live.streamtheworld.com:3690/JP_NEWSAAC_SC' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a JPNEWS_%q -l 9000 -s -d ./out &
 
-streamripper 'http://usa12.fastcast4u.com:5080/;' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a usa12_%q -l 8000 -s -d ./out &
+streamripper 'http://usa12.fastcast4u.com:5080/;' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a usa12_%q -l 9000 -s -d ./out &
 
-streamripper 'http://icecast-streaming.nice264.com/mallorca?.mp3' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a mallorca_%q -l 8000 -s -d ./out &
+streamripper 'http://icecast-streaming.nice264.com/mallorca?.mp3' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a mallorca_%q -l 9000 -s -d ./out &
 
-streamripper 'http://icestreaming.rai.it/1.mp3' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a rai_%q -l 8000 -s -d ./out &
+streamripper 'http://icestreaming.rai.it/1.mp3' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a rai_%q -l 9000 -s -d ./out &
 
-streamripper 'http://149.202.214.27:8888/radiotirana1' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a tirana_%q -l 8000 -s -d ./out &
+streamripper 'http://149.202.214.27:8888/radiotirana1' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a tirana_%q -l 9000 -s -d ./out &
 
-streamripper 'http://radios.vpn.sapo.pt/AO/radio5.mp3' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a sapo_%q -l 8000 -s -d ./out &
+streamripper 'http://radios.vpn.sapo.pt/AO/radio5.mp3' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a sapo_%q -l 9000 -s -d ./out &
 
-streamripper 'http://mp3.metroaudio1.stream.avstreaming.net:7200/bluefmaudio1' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a bluefmaudio1_%q -l 8000 -s -d ./out &
+streamripper 'http://mp3.metroaudio1.stream.avstreaming.net:7200/bluefmaudio1' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a bluefmaudio1_%q -l 9000 -s -d ./out &
 
-streamripper 'http://streaming.radioblogfm.net:8002/listen.pls' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a blogfm_%q -l 8000 -s -d ./out &
+streamripper 'http://streaming.radioblogfm.net:8002/listen.pls' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a blogfm_%q -l 9000 -s -d ./out &
 
-streamripper 'http://s2.netradiofm.com:9024/listen.pls' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a signos_%q -l 8000 -s -d ./out &
+streamripper 'http://s2.netradiofm.com:9024/listen.pls' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a signos_%q -l 9000 -s -d ./out &
 
-streamripper 'http://live02.rfi.fr/rfiafrique-64.mp3' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a rfi_%q -l 8000 -s -d ./out &
+streamripper 'http://live02.rfi.fr/rfiafrique-64.mp3' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a rfi_%q -l 9000 -s -d ./out &
 
-streamripper 'http://icecast.vrtcdn.be/radio1-high.mp3' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a vrt_%q -l 8000 -s -d ./out &
+streamripper 'http://icecast.vrtcdn.be/radio1-high.mp3' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a vrt_%q -l 9000 -s -d ./out &
 
-streamripper 'http://streaming06.hstbr.net:8202/live' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a globo_%q -l 8000 -s -d ./out &
+streamripper 'http://streaming06.hstbr.net:8202/live' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a globo_%q -l 9000 -s -d ./out &
 
-streamripper 'http://uk4-vn.mixstream.net:8142/;' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a balafon_%q -l 8000 -s -d ./out &
+streamripper 'http://uk4-vn.mixstream.net:8142/;' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a balafon_%q -l 9000 -s -d ./out &
 
-streamripper 'https://streams.radio.co/s419eaa350/listen' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a columbia_%q -l 8000 -s -d ./out &
+streamripper 'https://streams.radio.co/s419eaa350/listen' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a columbia_%q -l 9000 -s -d ./out &
 
-streamripper 'http://s2.voscast.com:11280/stream/1/' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a ubfm_%q -l 8000 -s -d ./out &
+streamripper 'http://s2.voscast.com:11280/stream/1/' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a ubfm_%q -l 9000 -s -d ./out &
 
-streamripper 'https://chai5she.cdn.dvmr.fr/rmcinfo' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a rmc_%q -l 8000 -s -d ./out &
+streamripper 'https://chai5she.cdn.dvmr.fr/rmcinfo' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a rmc_%q -l 9000 -s -d ./out &
 
-streamripper 'http://mp3lg3.scdn.arkena.com/10489/europe1.mp3' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a europe1_%q -l 8000 -s -d ./out &
+streamripper 'http://mp3lg3.scdn.arkena.com/10489/europe1.mp3' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a europe1_%q -l 9000 -s -d ./out &
 
-streamripper 'http://radiomars.ice.infomaniak.ch/radiomars-128.mp3' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a radiomars_%q -l 8000 -s -d ./out
+streamripper 'http://edge.espn.cdn.abacast.net/espn-deportes-48' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a espn_%q -l 9000 -s -d ./out &
+
+streamripper 'http://oe1shoutcast.sf.apa.at/stream.mp3' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a apa_%q -l 9000 -s -d ./out &
+
+streamripper 'http://video.tvr.by:8000/radio1' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a tvrby_%q -l 9000 -s -d ./out &
+
+streamripper 'http://79.143.184.143:8000/listen.pls' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a 79143_%q -l 9000 -s -d ./out &
+
+streamripper 'http://sh2.upx.com.br:8012/listen.pls' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a upx_%q -l 9000 -s -d ./out &
+
+streamripper 'http://unlimited1-us.dps.live/cooperativafm/mp3/icecast.audio' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a cooperativafm_%q -l 9000 -s -d ./out &
+
+streamripper 'http://rs1.radiostreamer.com:8000/;stream/1' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a rs1_%q -l 9000 -s -d ./out &
+
+streamripper 'https://icecast.teveo.icrt.cu/kHKL7tWd' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a icrt_%q -l 9000 -s -d ./out &
+
+streamripper 'https://drradio3-lh.akamaihd.net/i/p3_9@143506/index_256_a-p.m3u8' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a drradio3_%q -l 9000 -s -d ./out &
+
+streamripper 'http://icecast-streaming.nice264.com/ondaceroback' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a ondacero_%q -l 9000 -s -d ./out &
+
+streamripper 'http://sip-live.hds.adaptive.level3.net/hls-live/ruv-ras2/_definst_/live/stream1.m3u8' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a ruvras2_%q -l 9000 -s -d ./out &
+
+streamripper 'http://19253.live.streamtheworld.com/W_RADIOAAC.aac' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a W_RADIOAAC_%q -l 9000 -s -d ./out &
+
+streamripper 'http://17513.live.streamtheworld.com/RG690_SC' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a RG690_%q -l 9000 -s -d ./out &
+
+streamripper 'http://110.164.48.215:8000/smmradio' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a smmradio_%q -l 9000 -s -d ./out &
+
+streamripper 'http://stream1.sport890.com.uy:7914/;' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a sport890_%q -l 9000 -s -d ./out &
+
+streamripper 'https://d1a7butsko8nkd.cloudfront.net/sport.mp3' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a d1a7butsko8nkd_%q -l 9000 -s -d ./out &
+
+streamripper 'http://aler.org:8000/caracasam.aac' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a caracasam_%q -l 9000 -s -d ./out &
+
+streamripper 'http://radiomars.ice.infomaniak.ch/radiomars-128.mp3' -u 'iTunes/4.7 (Macintosh; N; PPC)' --xs-none -A -a radiomars_%q -l 9000 -s -d ./out
 
 rm -rf ./out/*.cue ./out/incomplete
 find ./out -size 0 | xargs -d '\n' rm -rf
