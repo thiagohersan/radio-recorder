c6222acb27363c9f86259767c1fc77c2956478be
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-07-12 01:20:33 -0300

    removes aws update from config script

--- config.sh
+++ config.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y zip wget at streamripper python-dev
+sudo apt-get install -y zip wget at streamripper
 sudo systemctl enable atd.service
 sudo cp /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
-
-wget https://bootstrap.pypa.io/get-pip.py
-sudo python get-pip.py
-
-su - admin -c "pip install awscli --upgrade --user"
-sudo rm /usr/local/bin/aws
-sudo ln -fs /home/admin/.local/bin/aws /usr/local/bin/aws
 
 wget https://raw.githubusercontent.com/thiagohersan/radio-recorder/master/record.sh
 chmod 700 record.sh
