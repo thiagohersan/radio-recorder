391388b1cf434a908075527838334d0a69b44eaa
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-07-12 23:39:15 -0300

    using absolute path for setting at commands

--- config.sh
+++ config.sh
@@ -10,5 +10,5 @@ chmod 700 record.sh
 chown admin:admin record.sh
 sudo mv record.sh /home/admin
 
-su - admin -c "at 10:50 071418 -f ~/record.sh"
-su - admin -c "at 11:50 071518 -f ~/record.sh"
+su - admin -c "at 10:50 071418 -f /home/admin/record.sh"
+su - admin -c "at 11:50 071518 -f /home/admin/record.sh"
